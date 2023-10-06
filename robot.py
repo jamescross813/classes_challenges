3b696a476b6fc00f17aa6e376de4515399878596
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:46:05 -0500

    tidy up and bug fix for calling class var

--- robot.py
+++ robot.py
@@ -4,14 +4,14 @@ class DriveBot:
     all_disabled =False
     latitude = -999999
     longitude = -999999
-    robot_count=0
+    robot_count = 0
 
     def __init__(self, motor_speed=0, direction=180, sensor_range=10):
         self.motor_speed = motor_speed
         self.direction = direction
         self.sensor_range = sensor_range
-        robot_count += 1
-        self.id = robot_count
+        self.id = DriveBot.robot_count + 1
+        
     
     def control_bot(self, new_speed, new_direction):
         self.motor_speed = new_speed
