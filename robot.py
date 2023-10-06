149811c738070b322d09b17883b919fca5dc2391
jamescross813 <crossjames813@gmail.com>
2023-10-06 11:24:16 -0500

    instance variables added

--- robot.py
+++ robot.py
@@ -1,7 +1,9 @@
 # Define the DriveBot class here!
 class DriveBot:
-    def __init__():
-        pass
+    def __init__(self):
+        self.motor_speed = 0
+        self.direction = 0
+        self.sensor_range = 0
 # print(robot_1.motor_speed)
 # print(robot_1.direction)
 # print(robot_1.sensor_range)
